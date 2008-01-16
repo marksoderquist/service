7f9d6ba1c058f82d774ea68cbd377da5c6c2ec15
Mark Soderquist <mark@soderquist.net>
2008-01-16 04:45:30 +0000

    Enable server service write test.

--- source/test/java/com/parallelsymmetry/service/ServerServiceTest.java
+++ source/test/java/com/parallelsymmetry/service/ServerServiceTest.java
@@ -1,10 +1,12 @@
 package com.parallelsymmetry.service;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 import junit.framework.TestCase;
 
 import com.parallelsymmetry.util.Log;
 
-// TODO One of these tests is causing Hudson to hang on occasion.
 public class ServerServiceTest extends TestCase {
 
 	private static final int PORT = 23423;
@@ -73,23 +75,63 @@ public class ServerServiceTest extends TestCase {
 		assertFalse( "Service is not stopped.", service.isRunning() );
 	}
 
-	//	public void testWrite() throws Exception {
-	//		ServerService server = new ServerService( PORT );
-	//		server.startAndWait();
-	//
-	//		SocketService service = new SocketService( server.getLocalPort() );
-	//		assertFalse( "Service should not be running.", service.isRunning() );
-	//		service.startAndWait();
-	//		assertTrue( "Service is not running.", service.isRunning() );
-	//
-	//		String message = "Test message.";
-	//		service.getOutputStream().write( message.getBytes( Charset.forName( "US-ASCII" ) ) );
-	//		assertEquals( "Incorrect message.", message, server.getMessage( message.length() ) );
-	//
-	//		service.stopAndWait();
-	//		assertFalse( "Service is not stopped.", service.isRunning() );
-	//
-	//		server.stopAndWait();
-	//	}
+	public void testWrite() throws Exception {
+		MockServer server = new MockServer( PORT );
+		server.startAndWait();
+
+		SocketService service = new SocketService( server.getLocalPort() );
+		assertFalse( "Service should not be running.", service.isRunning() );
+		service.startAndWait();
+		assertTrue( "Service is not running.", service.isRunning() );
+
+		String message = "Test message.";
+		service.getOutputStream().write( message.getBytes( Charset.forName( "US-ASCII" ) ) );
+		assertEquals( "Incorrect message.", message, server.getMessage( message.length() ) );
+
+		service.stopAndWait();
+		assertFalse( "Service is not stopped.", service.isRunning() );
+
+		server.stopAndWait();
+	}
+
+	private static final class MockServer extends ServerService {
+
+		public MockServer() {
+			super();
+		}
+
+		public MockServer( int port ) {
+			super( port );
+		}
+
+		public MockServer( String host, int port ) {
+			super( host, port );
+		}
+
+		public MockServer( String name, String host, int port ) {
+			super( name, host, port );
+		}
+
+		public MockServer( String name ) {
+			super( name );
+		}
+
+		public String getMessage( int length ) {
+			StringBuilder builder = new StringBuilder();
+
+			for( int index = 0; index < length; index++ ) {
+				try {
+					int data = getInputStream().read();
+					if( data < 0 ) return builder.toString();
+					builder.append( (char)data );
+				} catch( IOException exception ) {
+					Log.write( exception );
+				}
+			}
+
+			return builder.toString();
+		}
+
+	}
 
 }
