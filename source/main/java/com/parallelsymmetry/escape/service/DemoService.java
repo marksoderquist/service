f03b3429620d92659668596553d0da789460ec8a
Mark Soderquist <mark@soderquist.net>
2011-01-21 21:36:32 +0000

    Update demo service.

--- source/main/java/com/parallelsymmetry/escape/service/DemoService.java
+++ source/main/java/com/parallelsymmetry/escape/service/DemoService.java
@@ -1,5 +1,7 @@
 package com.parallelsymmetry.escape.service;
 
+import java.util.logging.Level;
+
 import com.parallelsymmetry.escape.utility.Parameters;
 import com.parallelsymmetry.escape.utility.log.Log;
 
@@ -7,8 +9,7 @@ public class DemoService extends Service {
 
 	public static final void main( String[] commands ) {
 		DemoService service = new DemoService();
-		System.err.println( "Home: " + service.getHomeFolder() );
-		service.call( service.getHomeFolder() == null ? new String[] { "-log.tag", "-log.level", "trace" } : commands );
+		service.call( service.getHomeFolder() == null ? new String[] { "-log.tag" } : commands );
 	}
 
 	@Override
@@ -18,16 +19,23 @@ public class DemoService extends Service {
 	protected void process( Parameters parameters ) throws Exception {
 		// -------------------0--------1---------2---------3---------4---------5---------6---------7---------8
 		// -------------------12345678901234567890123456789012345678901234567890123456789012345678901234567890
-		Log.write( Log.NONE );
-		Log.write( Log.NONE, "Welcome to the Demo Service. The Demo service is provided for reference" );
-		Log.write( Log.NONE, "purposes only. Please note the Demo Service automatically stops regardless" );
-		Log.write( Log.NONE, "of what parameters are specified on the command line." );
-		Log.write( Log.NONE );
+		Log.write();
+		Log.write( "Welcome to the Demo Service. The Demo service is provided for reference" );
+		Log.write( "purposes only. Please note the Demo Service automatically stops regardless" );
+		Log.write( "of what parameters are specified on the command line." );
+		Log.write();
+
+		Level level = Log.getLevel();
+		Log.setLevel( Log.ALL );
+		Log.write( "Sample log messages:" );
 		Log.write( Log.ERROR, "Error message." );
 		Log.write( Log.WARN, "Warning message." );
 		Log.write( Log.INFO, "Information message." );
 		Log.write( Log.TRACE, "Trace message." );
 		Log.write( Log.DEBUG, "Debug message." );
+		Log.write();
+		Log.setLevel( level );
+
 		call( new String[] { "-stop" } );
 	}
 
