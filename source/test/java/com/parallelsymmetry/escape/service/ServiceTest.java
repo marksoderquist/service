06cad5d2dfeda2b804259219245015ad897f6e59
Mark Soderquist <mark@soderquist.net>
2012-02-04 16:13:43 -0700

    Update notice format.

--- source/test/java/com/parallelsymmetry/escape/service/ServiceTest.java
+++ source/test/java/com/parallelsymmetry/escape/service/ServiceTest.java
@@ -347,12 +347,17 @@ public class ServiceTest extends BaseTestCase {
 	private void assertCommandLineHeader( String name, LineParser parser ) {
 		int currentYear = Calendar.getInstance( TimeZone.getTimeZone( "UTC" ) ).get( Calendar.YEAR );
 
-		assertEquals( TextUtil.pad( 60, '-' ), parser.next() );
+		String notice = "Mock Service comes with ABSOLUTELY NO WARRANTY. This is open software, and you are welcome to redistribute it under certain conditions.";
+
+		LineParser noticeLines = new LineParser( TextUtil.reline( notice, 75 ) );
+
+		assertEquals( TextUtil.pad( 75, '-' ), parser.next() );
 		assertEquals( name + " " + MOCK_RELEASE, parser.next() );
 		assertEquals( "(C) 1973-" + currentYear + " Parallel Symmetry All rights reserved.", parser.next() );
 		assertEquals( "", parser.next() );
-		assertEquals( "Mock Service comes with ABSOLUTELY NO WARRANTY. This is open software,", parser.next() );
-		assertEquals( "and you are welcome to redistribute it under certain conditions.", parser.next() );
+		while( noticeLines.more() ) {
+			assertEquals( noticeLines.next(), parser.next() );
+		}
 		assertEquals( "", parser.next() );
 	}
 
