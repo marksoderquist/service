13f08f92ab2950ff38237858d83c4693b306ea56
Mark Soderquist <mark@soderquist.net>
2012-06-21 15:16:45 -0600

    Continue work on module tool.

--- source/main/java/com/parallelsymmetry/escape/service/update/DescriptorDownload.java
+++ source/main/java/com/parallelsymmetry/escape/service/update/DescriptorDownload.java
@@ -10,7 +10,7 @@ import com.parallelsymmetry.escape.utility.Descriptor;
 import com.parallelsymmetry.escape.utility.task.Task;
 import com.parallelsymmetry.escape.utility.task.TaskListener;
 
-final class DescriptorDownload extends Task<Descriptor> {
+public final class DescriptorDownload extends Task<Descriptor> {
 
 	private DownloadTask task;
 
@@ -43,10 +43,7 @@ final class DescriptorDownload extends Task<Descriptor> {
 	public Descriptor execute() throws Exception {
 		Download download = task.execute();
 		if( download == null ) return null;
-
-		Descriptor descriptor = new Descriptor( new FileInputStream( download.getTarget() ) );
-
-		return descriptor;
+		return new Descriptor( new FileInputStream( download.getTarget() ) );
 	}
 
 }
