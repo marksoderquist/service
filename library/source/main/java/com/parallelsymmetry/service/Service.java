4d145eb9d68bfbdcce6011f660d34e8103b277a1
Mark Soderquist <mark@soderquist.net>
2014-05-14 16:44:18 -0600

    Fix opening resource from command line does not resolve resource

--- library/source/main/java/com/parallelsymmetry/service/Service.java
+++ library/source/main/java/com/parallelsymmetry/service/Service.java
@@ -894,7 +894,7 @@ public abstract class Service extends Agent implements ServiceProduct {
 				Log.write( Log.TRACE, "Connected to peer: " + peer );
 
 				ObjectOutputStream output = new ObjectOutputStream( socket.getOutputStream() );
-				output.writeObject( parameters.getOriginalCommands() );
+				output.writeObject( parameters.getResolvedCommands() );
 				output.flush();
 				Log.write( Log.TRACE, "Parameters sent to peer." );
 			} catch( ConnectException exception ) {
