6d29e4ce0ce1d556248fd42b26fc7265aed76ef5
Mark Soderquist <mark@soderquist.net>
2014-08-13 19:01:08 -0600

    Shorten the JvmSureStop timeout to 2 seconds

--- library/source/main/java/com/parallelsymmetry/service/JvmSureStop.java
+++ library/source/main/java/com/parallelsymmetry/service/JvmSureStop.java
@@ -4,6 +4,12 @@ import com.parallelsymmetry.utility.log.Log;
 
 public class JvmSureStop extends Thread {
 
+	/**
+	 * The amount of time to give the JVM to exit cleanly. After this amount of
+	 * time the JVM is halted by calling Runtime.getRuntime().halt().
+	 */
+	public static final int JVM_SURE_STOP_DELAY = 2000;
+
 	public JvmSureStop() {
 		setDaemon( true );
 	}
@@ -11,12 +17,11 @@ public class JvmSureStop extends Thread {
 	@Override
 	public void run() {
 		try {
-			Thread.sleep( 5000 );
+			Thread.sleep( JVM_SURE_STOP_DELAY );
 		} catch( InterruptedException exception ) {
 			return;
 		}
 		Log.write( Log.ERROR, "JVM did not exit cleanly. Halting now!" );
-		System.err.println( "JVM did not exit cleanly. Halting now!" );
 		Runtime.getRuntime().halt( -1 );
 	}
 
