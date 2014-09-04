9a5a3ffa94a4b1bb2e3727f173e3a127380945d7
Mark Soderquist <mark@soderquist.net>
2014-09-04 14:32:28 -0600

    Add thread information to the output of the JvmSureStop class

--- library/source/main/java/com/parallelsymmetry/service/JvmSureStop.java
+++ library/source/main/java/com/parallelsymmetry/service/JvmSureStop.java
@@ -10,7 +10,7 @@ public class JvmSureStop extends Thread {
 	 * The amount of time to give the JVM to exit cleanly. After this amount of
 	 * time the JVM is halted by calling Runtime.getRuntime().halt().
 	 */
-	public static final int JVM_SURE_STOP_DELAY = 500;
+	public static final int JVM_SURE_STOP_DELAY = 5000;
 
 	public JvmSureStop() {
 		super( "JVM Sure Stop" );
