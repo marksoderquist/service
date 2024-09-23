134fef46e3d0f2759e5c502d51dfbf1ec82f6dd8
msoderquist <msoderquist@mastercontrol.com>
2024-09-22 18:54:27 -0600

    Add module-info.java

---
+++ source/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.parallelsymmetry.service {
+	// Compile time only
+
+	// Compile and runtime
+	requires com.parallelsymmetry.updater;
+	requires com.parallelsymmetry.utility;
+	requires java.management;
+	requires java.logging;
+	requires java.desktop;
+}
